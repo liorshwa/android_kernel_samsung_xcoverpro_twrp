9fe84a1a175c419eb692a52380b817ef63df2625
User <user@example.com>
2021-01-18 17:02:01 +0200

    SElinux: Use kernel linux/socket.h for genheaders and mdp

--- security/selinux/include/classmap.h
+++ security/selinux/include/classmap.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <linux/capability.h>
+#include <linux/socket.h>
 
 #define COMMON_FILE_SOCK_PERMS "ioctl", "read", "write", "create", \
     "getattr", "setattr", "lock", "relabelfrom", "relabelto", "append", "map"
@@ -243,4 +244,7 @@ struct security_class_mapping secclass_map[] = {
 	{ NULL }
   };
 
+#if PF_MAX > 44
+#error New address family defined, please update secclass_map.
+#endif
 
